2c26bd107ddacc73e2d4d6156e92062323a42ee2
root <root@skat-office>
2017-10-23 15:12:55 +0300

    [fix] Unnecesary dependencies

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 const Package = require('./Package')
 
-let skatServer = new Package('skat-server-cloud')
-let skatAdmin = new Package('skat-maintenance')
-
-let start = async () => {
-  await skatServer.loadMeta()
-  await skatAdmin.loadMeta()
-  if (skatAdmin.metaLoaded) {
-    console.log(skatAdmin.version, skatAdmin.avaialableVersion)
-  }
-}
-
-start()
-
+module.exports = Package
