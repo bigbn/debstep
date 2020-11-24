4c5981dc444f14d6e8bad47390f9242c4dd73db5
izhastron <izhastron@gmail.com>
2020-11-24 07:06:52 +0000

    [upd] Updated apt binding

--- binding.gyp
+++ binding.gyp
@@ -4,6 +4,8 @@
       "target_name": "bindings",
       "sources": [ "bindings.cpp" ],
       "library_dirs": [ "/usr/include/"],
+      "cflags_cc!": [ "-fno-rtti" ],
+      "cflags!": [ "-fno-rtti" ],
       "libraries": [ "-lapt-pkg" ]      
     }
   ]
--- bindings.cpp
+++ bindings.cpp
@@ -1,23 +1,60 @@
 #include <node_api.h>
+#include <iostream>
+#include <string>
+#include <typeinfo>
 #include <apt-pkg/cachefile.h>
 #include <apt-pkg/pkgcache.h>
-#include <apt-pkg/pkgrecords.h>
 
 using namespace std;
 
 namespace demo {
-    
+  static pkgPolicy *policy;
+  pkgRecords::Parser &LookupParser(pkgRecords &Recs, pkgCache::VerIterator const &V, pkgCache::VerFileIterator &Vf) /*{{{*/
+  {
+    Vf = V.FileList();
+    for (; Vf.end() == false; ++Vf)
+        if ((Vf.File()->Flags & pkgCache::Flag::NotSource) == 0)
+    break;
+    if (Vf.end() == true)
+        Vf = V.FileList();
+    return Recs.Lookup(Vf);
+  }
+  static void coutPackage(const pkgCache::PkgIterator &P, const pkgCacheFile &cacheFile)
+  {
+    pkgCache::VerIterator ver_iterator = P.CurrentVer();
+    if(!ver_iterator || !ver_iterator.FileList())
+        return;
+    pkgRecords Recs(cacheFile);
+    pkgCache::VerFileIterator vf_iterator = ver_iterator.FileList();
+    auto &Parser = LookupParser(Recs, ver_iterator, vf_iterator);
+    char const *Start, *Stop;
+    Parser.GetRec(Start, Stop);
+    size_t const Length = Stop - Start;
+    pkgTagSection Tags;
+    Tags.Scan(Start, Length, true);
+    std::string key = "XB-Nginx-Template";
+    std::cout << Tags.FindS("Package") << std::endl;
+    std::cout << SizeToStr(Tags.FindULL("Installed-Size") * 1024).c_str() << std::endl;
+  }
   napi_value Method(napi_env env, napi_callback_info args) {
+    
     napi_value greeting;
     napi_status status;
+    pkgInitConfig(*_config);
+    pkgInitSystem(*_config, _system);
 
-    pkgCacheFile cache_file;
-    pkgCache* cache = cache_file.GetPkgCache();
-
-    for (pkgCache::PkgIterator package = cache->PkgBegin(); !package.end(); package++) {
-        std::cout << package.Name() << std::endl;
+    pkgCacheFile cachefile;
+    pkgCache *cache = cachefile.GetPkgCache();
+    policy = cachefile.GetPolicy();
+    pkgCache::Header* head = cache->HeaderP;
+    if (cache == NULL || _error->PendingError()) {
+        _error->DumpErrors();
+        return nullptr;
     }
 
+    for (pkgCache::GrpIterator grp = cache->GrpBegin(); grp != cache->GrpEnd(); grp++)
+       for (pkgCache::PkgIterator p = grp.PackageList(); !p.end(); p = grp.NextPkg(p))
+          coutPackage(p, cachefile);
     status = napi_create_string_utf8(env, "world", NAPI_AUTO_LENGTH, &greeting);
     if (status != napi_ok) return nullptr;
     return greeting;
