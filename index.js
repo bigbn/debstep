874498a66df6f5d26f5ff52afee2b28c5597b095
izhastron <izhastron@gmail.com>
2020-11-25 06:50:52 +0000

    [upd] Update functional

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const Package = require('./Package')
 const PackageManager = require('./PackageManager')
 const bindings = require('./build/Release/bindings');
 
-console.log(bindings.getPackageTags("vim", ["Package", "XB-Nginx-Template", "Version", "Description"]))
-
+console.log(bindings.getPackageTags("npm", ["Package", "XB-Nginx-Template", "Version", "Description"]))
+console.log(bindings.getPackageCandidateVersion("npm"))
+console.log(bindings.getPackageCurrentVersion("npm"))
 let pm = new PackageManager()
 
 module.exports = {Package, PackageManager}
