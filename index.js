7964f2487086704072d5ed8e4f52d3e7a79eaf98
Pavel Shabardin <pavel@scat.su>
2020-11-25 10:07:01 +0000

    [rfc] Flow, Standard, Documentation and src/lib folders

--- index.js
+++
@@ -1,8 +0,0 @@
-const Package = require('./Package')
-const PackageManager = require('./PackageManager')
-
-let pm = new PackageManager()
-const package = new Package('vim')
-console.log(package.getTagsSync())
-
-module.exports = {Package, PackageManager}
---
+++ scripts/copy.js
@@ -0,0 +1,33 @@
+import { basename, resolve } from 'path'
+import { copy, writeFile } from 'fs-extra'
+
+async function copyFile (file) {
+  const fileName = basename(file)
+  const filePath = resolve(__dirname, '../lib/', fileName)
+
+  await copy(file, filePath)
+
+  console.log(`Copied ${file} to ${filePath}`)
+}
+
+async function createPackageFile () {
+  const oldPckgPath = resolve(__dirname, '../package.json')
+  const oldPckgData = require(oldPckgPath)
+
+  delete oldPckgData.private
+  // delete oldPckgData.scripts
+  delete oldPckgData.devDependencies
+
+  const newPckgPath = resolve(__dirname, '../lib/package.json')
+  const newPckgData = Object.assign(oldPckgData, { main: './index.js' })
+  await writeFile(newPckgPath, JSON.stringify(newPckgData), 'utf8')
+
+  console.log(`Created package.json in ${newPckgPath}`)
+}
+
+async function run () {
+  await ['README.md', 'LICENSE'].map(file => copyFile(file))
+  await createPackageFile()
+}
+
+run()
